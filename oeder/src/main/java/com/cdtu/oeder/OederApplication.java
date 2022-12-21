a4b5b1a4c7e1ca5da1e5ada1231a44f55d916ee0
Sakura <1950622837@qq.com>
2022-12-21 18:40:58 +0800

    项目初始化

---
+++ oeder/src/main/java/com/cdtu/oeder/OederApplication.java
@@ -0,0 +1,13 @@
+package com.cdtu.oeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OederApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OederApplication.class, args);
+    }
+
+}
---
+++ oeder/src/test/java/com/cdtu/oeder/OederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdtu.oeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OederApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
